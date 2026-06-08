5edd942a9c7b7c61bab63e844f43d6274faa2c79
shyamcb <shyamboopathi2006@gmail.com>
2026-06-08 14:30:26 +0530

    Delete goal

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -21,3 +21,52 @@ exports.createGoal = async (req, res) => {
     });
   }
 };
+
+exports.getGoals = async (req, res) => {
+  try {
+    const goal = await Goal.find({ user: req.user._id });
+
+    res.status(200).json({
+      status: "success",
+      results: goal.length,
+      data: {
+        goal,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+exports.deleteGoal = async (req, res) => {
+  try {
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Goal not found",
+      });
+    }
+    if (goal.user.toString() !== req.user.id.toString()) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Not Authorized",
+      });
+    }
+    await Goal.deleteOne();
+
+    res.status(200).json({
+      status: "success",
+      message: "Goal deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -5,5 +5,7 @@ const { protect } = require("../middleware/authMiddleware");
 const controllers = require("../controllers/goalController");
 
 router.post("/", protect, controllers.createGoal);
+router.get("/", protect, controllers.getGoals);
+router.delete("/:id", protect, controllers.deleteGoal);
 
 module.exports = router;
