34fb97cafdcc659d3e80d27806095e85d2db7554
shyamcb <shyamboopathi2006@gmail.com>
2026-06-11 19:59:23 +0530

    Create roadmap API

---
+++ server/controllers/roadmapController.js
@@ -0,0 +1,26 @@
+const Roadmap = require("../models/Roadmap");
+
+exports.createRoadmap = async (req, res) => {
+  try {
+    const { title, description, steps } = req.body;
+    console.log(req.body);
+    const roadmap = await Roadmap.create({
+      title,
+      description,
+      steps,
+      user: req.user._id,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        roadmap,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: ree.message,
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const User = require("./models/User");
 const authRoutes = require("./routes/authRoutes");
 const goalRoutes = require("./routes/goalRoutes");
+const roadmapRoutes = require("./routes/roadmapRoutes");
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/goals", goalRoutes);
+app.use("/api/roadmaps", roadmapRoutes);
 
 const PORT = process.env.PORT || 5000;
 
