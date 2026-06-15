dcc01bbcdbace44121984a836f13c487318d8045
shyamcb <shyamboopathi2006@gmail.com>
2026-06-15 18:34:07 +0530

    Add dashboard routes

--- server/server.js
+++ server/server.js
@@ -7,15 +7,18 @@ const User = require("./models/User");
 const authRoutes = require("./routes/authRoutes");
 const goalRoutes = require("./routes/goalRoutes");
 const roadmapRoutes = require("./routes/roadmapRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 dotenv.config();
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/goals", goalRoutes);
 app.use("/api/roadmaps", roadmapRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
