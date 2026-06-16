455483db6c2481f07917bbdb577cb23d6769d421
shyamcb <shyamboopathi2006@gmail.com>
2026-06-16 19:26:07 +0530

    Refactor controllers using asyncHandler

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,75 +1,62 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({
-        status: "fail",
-        message: "User already exist",
-      });
-    }
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    res.status(201).json({
-      status: "success",
-      message: "User registered successfully",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
+exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
       status: "fail",
-      message: err.message,
+      message: "User already exist",
     });
   }
-};
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
-exports.loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  res.status(201).json({
+    status: "success",
+    message: "User registered successfully",
+    data: {
+      user,
+    },
+  });
+});
 
-    if (!user) {
-      return res.status(404).json({
-        status: "fail",
-        message: "User not found",
-      });
-    }
+exports.loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({
-        status: "fail",
-        message: "Password wrong",
-      });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN, {
-      expiresIn: "7d",
-    });
-
-    res.status(200).json({
-      status: "success",
-      message: "Login successful",
-      token,
-    });
-  } catch (err) {
-    res.status(500).json({
+  if (!user) {
+    return res.status(404).json({
       status: "fail",
-      message: err.message,
+      message: "User not found",
     });
   }
-};
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Password wrong",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN, {
+    expiresIn: "7d",
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "Login successful",
+    token,
+  });
+});
 
 exports.getMe = async (req, res) => {
   res.status(200).json({
--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,159 +1,123 @@
 const Goal = require("../models/Goal");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.createGoal = async (req, res) => {
-  try {
-    const { title } = req.body;
-    const goal = await Goal.create({
-      title,
-      user: req.user._id,
-    });
+exports.createGoal = asyncHandler(async (req, res) => {
+  const { title } = req.body;
+  const goal = await Goal.create({
+    title,
+    user: req.user._id,
+  });
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        goal,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
+  res.status(201).json({
+    status: "success",
+    data: {
+      goal,
+    },
+  });
+});
+
+exports.getGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.find({ user: req.user._id }).populate(
+    "user",
+    "name email",
+  );
+
+  res.status(200).json({
+    status: "success",
+    results: goal.length,
+    data: {
+      goal,
+    },
+  });
+});
+
+exports.deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    return res.status(404).json({
       status: "fail",
-      message: err.message,
+      message: "Goal not found",
     });
   }
-};
-
-exports.getGoals = async (req, res) => {
-  try {
-    const goal = await Goal.find({ user: req.user._id }).populate(
-      "user",
-      "name email",
-    );
-
-    res.status(200).json({
-      status: "success",
-      results: goal.length,
-      data: {
-        goal,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
+  if (goal.user.toString() !== req.user.id.toString()) {
+    return res.status(400).json({
       status: "fail",
-      message: err.message,
+      message: "Not Authorized",
     });
   }
-};
 
-exports.deleteGoal = async (req, res) => {
-  try {
-    const goal = await Goal.findById(req.params.id);
+  await goal.deleteOne();
 
-    if (!goal) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Goal not found",
-      });
-    }
-    if (goal.user.toString() !== req.user.id.toString()) {
-      return res.status(400).json({
-        status: "fail",
-        message: "Not Authorized",
-      });
-    }
+  res.status(200).json({
+    status: "success",
+    message: "Goal deleted successfully",
+  });
+});
 
-    await Goal.deleteOne();
+exports.updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      message: "Goal deleted successfully",
-    });
-  } catch (err) {
-    res.status(500).json({
+  if (!goal) {
+    return res.status(404).json({
       status: "fail",
-      message: err.message,
+      message: "Goal not found",
     });
   }
-};
 
-exports.updateGoal = async (req, res) => {
-  try {
-    const goal = await Goal.findById(req.params.id);
-
-    if (!goal) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Goal not found",
-      });
-    }
-
-    if (goal.user.toString() !== req.user.id.toString()) {
-      return res.status(400).json({
-        status: "fail",
-        message: "Not Authorized",
-      });
-    }
-
-    goal.title = req.body.title || goal.title;
-
-    if (req.body.status) {
-      goal.status = req.body.status;
-    }
-
-    if (req.body.status !== undefined) {
-      goal.progress = req.body.progress;
-    }
-
-    if (goal.progress === 100) {
-      goal.status = "completed";
-    }
-
-    await goal.save();
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        goal,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
+  if (goal.user.toString() !== req.user.id.toString()) {
+    return res.status(400).json({
       status: "fail",
-      message: err.message,
+      message: "Not Authorized",
     });
   }
-};
 
-exports.getGoalStats = async (req, res) => {
-  try {
-    const goals = await Goal.find({ user: req.user._id });
+  goal.title = req.body.title || goal.title;
 
-    const totalGoals = goals.length;
-
-    const completedGoals = goals.filter(
-      (goal) => goal.status === "completed",
-    ).length;
-
-    const pendingGoals = goals.filter(
-      (goal) => goal.status === "pending",
-    ).length;
-
-    const inProgressGoals = goals.filter(
-      (goal) => goal.status === "in-progress",
-    ).length;
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        totalGoals,
-        inProgressGoals,
-        pendingGoals,
-        completedGoals,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err.message,
-    });
+  if (req.body.status) {
+    goal.status = req.body.status;
   }
-};
+
+  if (req.body.status !== undefined) {
+    goal.progress = req.body.progress;
+  }
+
+  if (goal.progress === 100) {
+    goal.status = "completed";
+  }
+
+  await goal.save();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      goal,
+    },
+  });
+});
+
+exports.getGoalStats = asyncHandler(async (req, res) => {
+  const goals = await Goal.find({ user: req.user._id });
+
+  const totalGoals = goals.length;
+
+  const completedGoals = goals.filter(
+    (goal) => goal.status === "completed",
+  ).length;
+
+  const pendingGoals = goals.filter((goal) => goal.status === "pending").length;
+
+  const inProgressGoals = goals.filter(
+    (goal) => goal.status === "in-progress",
+  ).length;
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      totalGoals,
+      inProgressGoals,
+      pendingGoals,
+      completedGoals,
+    },
+  });
+});
