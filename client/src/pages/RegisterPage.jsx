506e11f5f77d7276496074d3775602b9691345fb
shyamcb <shyamboopathi2006@gmail.com>
2026-06-19 20:59:51 +0530

    Store user in DB through register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,68 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../services/authService";
 import "../styles/auth.css";
 
 function RegisterPage() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await registerUser(formData);
+
+      console.log(data);
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error.response?.data);
+    }
+  };
+
   return (
     <div className="auth-container">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmit}>
         <h1>Register</h1>
 
-        <input type="text" placeholder="Name" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
 
-        <input type="email" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
 
-        <input type="password" placeholder="Password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
 
-        <button type="submit">Register</button>
+        <button>Register</button>
 
         <p>
           Already have an account? <Link to="/login">Login</Link>
