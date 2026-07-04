3a2bc0a3ba05c79dd8879bb6a9c823520d8028e7
shyamcb <shyamboopathi2006@gmail.com>
2026-07-04 22:04:35 +0530

    Style dashboard layout

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -2,10 +2,11 @@ import Sidebar from "../components/Sidebar";
 
 function DashboardLayout({ children }) {
   return (
-    <div className="dashboard-layout">
+    <div className="min-h-screen bg-[#fafafa] text-slate-900 lg:flex">
       <Sidebar />
-
-      <main className="dashboard-main">{children}</main>
+      <main className="flex-1 px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
+        <div className="mx-auto max-w-7xl">{children}</div>
+      </main>
     </div>
   );
 }
